06b7a119ffdd505932b149c61519532591403ba6
TrevorSTroxel <actroxel@gmail.com>
2016-10-25 11:36:05 -0700

    Add files via upload

---
+++ code1/Text2.java
@@ -0,0 +1,23 @@
+class Text2 {
+
+	public static void main (String a[]){
+		int i, ci;   // interger i, ci
+		char c;
+			
+		i= 7;
+		c= 'A';
+		String s = "Whats 9+10? 21?";;//define String s
+		System.out.println("*************************");
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(s);
+		System.out.println("*************************");
+	//do some math on the varaibles
+		i = i % 7;
+		ci = (int)c;
+		System.out.println("*************************");
+		System.out.println("i = " + i);
+		System.out.println("Now ci = "+ci);
+	
+	}
+}
