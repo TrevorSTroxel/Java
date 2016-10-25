7093b6936d97a0d5705b915e5f03d7c54f306b92
TrevorSTroxel <actroxel@gmail.com>
2016-10-25 11:48:25 -0700

    Add files via upload

---
+++ code1/Hello.java
@@ -0,0 +1,8 @@
+// save this file in a folder called java you have created in home
+// Hello2.java
+
+class Hello {
+	public static void main (String a[]){
+			System.out.print("Hello 2 world!");
+	}
+}
